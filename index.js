43bf9cdc991394fc7e86c482bec331d77f100128
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-21 10:40:56 +0700

    abcd

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const PhepTinh = require('./PhepTinh');
 
 const parser = bodyParser.urlencoded({ extended: false });
 
+class User {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
+const arrUsers = [
+    new User('pho', '123'),
+    new User('khoa', '321'),
+    new User('tien', '456')
+];
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -13,6 +25,14 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'));
 
 app.get('/tinh', (req, res) => res.render('tinh'));
+
+app.get('/admin', (req, res) => res.render('admin', { arrUsers })); //Route 1
+
+// app.get('/dangky', (req, res) => res.render('dangKy'));
+
+// app.post('dangky', parser, (req, res) => {
+
+// });
 
 app.post('/tinh', parser, (req, res) => {
     const { soA, soB, tenPt } = req.body;
