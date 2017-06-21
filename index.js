9d00f52f791e3f82cb0066d8ae4a4111dbdd8a9b
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-21 10:05:01 +0700

    receive data from post method

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const parser = bodyParser.urlencoded({ extended: false });
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('home'));
+
+app.post('/xuLy', parser, (req, res) => {
+    console.log(req.body);
+    res.send('Da nhan duoc request');
+});
 
 app.listen(3000, () => console.log('Server started!'));
